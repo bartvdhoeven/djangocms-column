a1065f24f4f0b4f588be9ace02979c6eb5ceb897
bartvdhoeven <bartvdhoeven@gmail.com>
2021-02-23 11:47:14 +0100

    Update 0004_column_add_class.py

--- djangocms_column/migrations/0004_column_add_class.py
+++ djangocms_column/migrations/0004_column_add_class.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.AddField(
-            model_name='columns',
+            model_name='column',
             name='add_class',
             field=models.CharField(blank=True, max_length=50, null=True, verbose_name='class'),
         ),
